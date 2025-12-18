55086a1c1c2f57b4902c7bcf7e9988bfe6b31712
MahmoudElshourbagy1 <mahmoudmera555@gmail.com>
2025-12-18 14:59:43 +0200

    implement a hoc for managing window states and add the frist windiw - terminal

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import {Tooltip} from "react-tooltip";
 import {dockApps} from "#constants";
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "#store/window.js";
 
 const Dock = () => {
+    const {openWindow , closeWindow , windows}= useWindowStore();
     const dockRef = useRef(null);
     useGSAP(()=>{
         const dock = dockRef.current;
@@ -43,7 +45,19 @@ const Dock = () => {
         }
     },[])
 
-    const toggleApp=(app)=>{};
+    const toggleApp=(app)=>{
+        if(!app.canOpen) return;
+        const window = windows[app.id];
+        if(!window) {
+            console.error(`Could not open window :${app.id}`)
+            return;
+        };
+        if(window.isOpen){
+            closeWindow(app.id);
+        }else{
+            openWindow(app.id);
+        }
+    };
     return (
         <section id="dock">
             <div ref={dockRef} className="dock-container">
